be7c552c07e867441db6e9d5302472905c20fb46
Nguyễn Minh Huy <minhhuy122001@gmail.com>
2023-09-23 16:11:51 +0700

    login ui ++

--- src/layouts/components/Header/Header.tsx
+++ src/layouts/components/Header/Header.tsx
@@ -9,7 +9,7 @@ import {
     MenuList,
     MenuItem,
     Avatar,
-    IconButton, Drawer,
+    IconButton, Drawer, Dialog, CardHeader, Card, CardBody, Input, Checkbox, CardFooter,
 } from "@material-tailwind/react";
 import {
     UserCircleIcon,
@@ -49,16 +49,66 @@ const profileMenuItems = [
 
 function ProfileMenu() {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
+    const [open, setOpen] = useState<boolean>(false);
     const closeMenu = () => setIsMenuOpen(false);
+    const handleOpen = () => setOpen((cur) => !cur);
 
     return (
         <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+            <button
+                onClick={handleOpen}
+                className="absolute right-[110px] bg-sky-400 rounded-2xl py-2 px-3 hover:bg-sky-500 text-white drop-shadow-sm"
+                color="blue">Đăng nhập
+            </button>
+            <Dialog
+                size="xs"
+                open={open}
+                handler={handleOpen}
+                className="bg-transparent shadow-none"
+            >
+                <Card className="mx-auto w-full max-w-[24rem]">
+                    <CardHeader
+                        variant="gradient"
+                        color="blue"
+                        className="mb-4 grid h-28 place-items-center"
+                    >
+                        <Typography variant="h3" color="white">
+                            Sign In
+                        </Typography>
+                    </CardHeader>
+                    <CardBody className="flex flex-col gap-4">
+                        <Input crossOrigin label="Email" size="lg" />
+                        <Input crossOrigin label="Password" size="lg" />
+                        <div className="-ml-2.5">
+                            <Checkbox crossOrigin label="Remember Me" />
+                        </div>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+                        <Button variant="gradient" onClick={handleOpen} fullWidth>
+                            Sign In
+                        </Button>
+                        <Typography variant="small" className="mt-6 flex justify-center">
+                            Don&apos;t have an account?
+                            <Typography
+                                as="a"
+                                href="#signup"
+                                variant="small"
+                                color="blue"
+                                className="ml-1 font-bold"
+                                onClick={handleOpen}
+                            >
+                                Sign up
+                            </Typography>
+                        </Typography>
+                    </CardFooter>
+                </Card>
+            </Dialog>
+
             <MenuHandler>
                 <Button
                     variant="text"
                     color="blue-gray"
-                    className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+                    className="flex relative items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
                 >
                     <Avatar
                         variant="circular"
@@ -109,67 +159,66 @@ function ProfileMenu() {
 }
 
 
-
 function Header() {
     const [open, setOpen] = React.useState(false);
 
     const openDrawer = () => setOpen(true);
     const closeDrawer = () => setOpen(false);
     return (
-      <nav className=''>
-          <Navbar fullWidth={true} variant='gradient' className="mx-auto lg:pl-6 w-full">
-              <div className="relative mx-auto flex items-center text-blue-gray-900">
-                  <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXGky50hqnXw4OspoYR8ENebsJzE6Cd1lYeBOBrIelVE4BfEQTUqNzz_7T1EB1TSgUag&usqp=CAU"
-                      alt="Your Logo"
-                      className="mr-4 h-12 w-auto"/>
+        <nav className=''>
+            <Navbar fullWidth={true} variant='gradient' className="mx-auto lg:pl-6 w-full">
+                <div className="relative mx-auto flex items-center text-blue-gray-900">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXGky50hqnXw4OspoYR8ENebsJzE6Cd1lYeBOBrIelVE4BfEQTUqNzz_7T1EB1TSgUag&usqp=CAU"
+                        alt="Your Logo"
+                        className="mr-4 h-12 w-auto"/>
 
-                  <IconButton
-                      size="sm"
-                      color="blue-gray"
-                      variant="text"
-                      onClick={openDrawer}
-                      className="ml-auto mr-2 lg:hidden"
-                  >
-                      <Bars2Icon className="h-6 w-6"/>
-                  </IconButton>
-                  <ProfileMenu/>
-              </div>
-          </Navbar>
-          <Drawer open={open} onClose={closeDrawer} className="p-4">
-              <div className="mb-6 flex items-center justify-between">
-                  <Typography variant="h5" color="blue-gray">
-                      Material Tailwind
-                  </Typography>
-                  <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
-                      <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={2}
-                          stroke="currentColor"
-                          className="h-5 w-5"
-                      >
-                          <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M6 18L18 6M6 6l12 12"
-                          />
-                      </svg>
-                  </IconButton>
-              </div>
-              <Typography color="gray" className="mb-8 pr-4 font-normal">
-                  Material Tailwind features multiple React and HTML components, all
-                  written with Tailwind CSS classes and Material Design guidelines.
-              </Typography>
-              <div className="flex gap-2">
-                  <Button size="sm">Get Started</Button>
-                  <Button size="sm" variant="outlined">
-                      Documentation
-                  </Button>
-              </div>
-          </Drawer>
-      </nav>
+                    <IconButton
+                        size="sm"
+                        color="blue-gray"
+                        variant="text"
+                        onClick={openDrawer}
+                        className="ml-auto mr-2 lg:hidden"
+                    >
+                        <Bars2Icon className="h-6 w-6"/>
+                    </IconButton>
+                    <ProfileMenu/>
+                </div>
+            </Navbar>
+            <Drawer open={open} onClose={closeDrawer} className="p-4">
+                <div className="mb-6 flex items-center justify-between">
+                    <Typography variant="h5" color="blue-gray">
+                        Material Tailwind
+                    </Typography>
+                    <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-5 w-5"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </IconButton>
+                </div>
+                <Typography color="gray" className="mb-8 pr-4 font-normal">
+                    Material Tailwind features multiple React and HTML components, all
+                    written with Tailwind CSS classes and Material Design guidelines.
+                </Typography>
+                <div className="flex gap-2">
+                    <Button size="sm">Get Started</Button>
+                    <Button size="sm" variant="outlined">
+                        Documentation
+                    </Button>
+                </div>
+            </Drawer>
+        </nav>
     );
 }
 
