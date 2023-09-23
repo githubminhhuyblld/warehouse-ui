a317253627c5408dfca7e9b7afb17492fb4f903a
Nguyễn Minh Huy <minhhuy122001@gmail.com>
2023-09-23 15:41:03 +0700

    sidebar ++

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,8 +1,35 @@
-import {FC, ReactNode} from 'react';
+import {FC, ReactNode, useState} from 'react';
+import {
+    Card,
+    Typography,
+    List,
+    ListItem,
+    ListItemPrefix,
+    ListItemSuffix,
+    Chip,
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
+    Alert,
+} from "@material-tailwind/react";
+import {
+    PresentationChartBarIcon,
+    ShoppingBagIcon,
+    UserCircleIcon,
+    Cog6ToothIcon,
+    InboxIcon,
+    PowerIcon,
+} from "@heroicons/react/24/solid";
+import {
+    ChevronRightIcon,
+    ChevronDownIcon,
+    CubeTransparentIcon,
+} from "@heroicons/react/24/outline";
 
 
 import Header from "~/layouts/components/Header/Header.tsx";
-import Footer from "~/layouts/components/Footer/Footer.tsx";
+
+// import Footer from "~/layouts/components/Footer/Footer.tsx";
 
 
 interface DefaultLayoutStylesProps {
@@ -10,17 +37,161 @@ interface DefaultLayoutStylesProps {
 }
 
 const DefaultLayout: FC<DefaultLayoutStylesProps> = ({children}) => {
+
+    const [open, setOpen] = useState(0);
+    const [openAlert, setOpenAlert] = useState(true);
+
+    const handleOpen = (value: number) => {
+        setOpen(open === value ? 0 : value);
+    };
     return <>
         <header className='header'>
             <Header/>
         </header>
-        <aside className='content'>
-            {children}
+        <aside className='content grid grid-cols-12 gap-4'>
+            <section className="hidden xl:block xl:col-span-2">
+                <Card className="h-[calc(100vh-2rem)]   w-full rounded-none p-2 shadow-blue-gray-900/5 mt-1 ">
+                    <List className="min-w-[210px]">
+                        <Accordion
+                            open={open === 1}
+                            icon={
+                                <ChevronDownIcon
+                                    strokeWidth={2.5}
+                                    className={` h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
+                                />
+                            }
+                        >
+                            <ListItem className="p-0" selected={open === 1}>
+                                <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
+                                    <ListItemPrefix>
+                                        <PresentationChartBarIcon className="h-5 w-5"/>
+                                    </ListItemPrefix>
+                                    <Typography color="blue-gray" className="mr-auto font-normal">
+                                        Dashboard
+                                    </Typography>
+                                </AccordionHeader>
+                            </ListItem>
+                            <AccordionBody className="py-1">
+                                <List className="p-0">
+                                    <ListItem>
+                                        <ListItemPrefix>
+                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5"/>
+                                        </ListItemPrefix>
+                                        Analytics
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemPrefix>
+                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5"/>
+                                        </ListItemPrefix>
+                                        Reporting
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemPrefix>
+                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5"/>
+                                        </ListItemPrefix>
+                                        Projects
+                                    </ListItem>
+                                </List>
+                            </AccordionBody>
+                        </Accordion>
+                        <Accordion
+                            open={open === 2}
+                            icon={
+                                <ChevronDownIcon
+                                    strokeWidth={2.5}
+                                    className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
+                                />
+                            }
+                        >
+                            <ListItem className="p-0" selected={open === 2}>
+                                <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
+                                    <ListItemPrefix>
+                                        <ShoppingBagIcon className="h-5 w-5"/>
+                                    </ListItemPrefix>
+                                    <Typography color="blue-gray" className="mr-auto font-normal">
+                                        E-Commerce
+                                    </Typography>
+                                </AccordionHeader>
+                            </ListItem>
+                            <AccordionBody className="py-1">
+                                <List className="p-0">
+                                    <ListItem>
+                                        <ListItemPrefix>
+                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5"/>
+                                        </ListItemPrefix>
+                                        Orders
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemPrefix>
+                                            <ChevronRightIcon strokeWidth={3} className="h-3 w-5"/>
+                                        </ListItemPrefix>
+                                        Products
+                                    </ListItem>
+                                </List>
+                            </AccordionBody>
+                        </Accordion>
+                        <hr className="my-2 border-blue-gray-50"/>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <InboxIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            Inbox
+                            <ListItemSuffix>
+                                <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full"/>
+                            </ListItemSuffix>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <UserCircleIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            Profile
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <Cog6ToothIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            Settings
+                        </ListItem>
+                        <ListItem>
+                            <ListItemPrefix>
+                                <PowerIcon className="h-5 w-5"/>
+                            </ListItemPrefix>
+                            Log Out
+                        </ListItem>
+                    </List>
+                    <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
+                        <CubeTransparentIcon className="mb-4 h-12 w-12"/>
+                        <Typography variant="h6" className="mb-1">
+                            Upgrade to PRO
+                        </Typography>
+                        <Typography variant="small" className="font-normal opacity-80">
+                            Upgrade to Material Tailwind PRO and get even more components, plugins, advanced features
+                            and premium.
+                        </Typography>
+                        <div className="mt-4 flex gap-3">
+                            <Typography
+                                as="a"
+                                href="#"
+                                variant="small"
+                                className="font-medium opacity-80"
+                                onClick={() => setOpenAlert(false)}
+                            >
+                                Dismiss
+                            </Typography>
+                            <Typography as="a" href="#" variant="small" className="font-medium">
+                                Upgrade Now
+                            </Typography>
+                        </div>
+                    </Alert>
+                </Card>
+            </section>
+            <section className="lg:col-span-12 xl:col-span-10 md:col-span-12 sm:col-span-12 col-span-12">
+                {children}
+            </section>
         </aside>
         <footer className='footer'>
-            <Footer/>
+            {/*<Footer/>*/}
         </footer>
-
     </>;
 };
 
--- src/layouts/components/Header/Header.tsx
+++ src/layouts/components/Header/Header.tsx
@@ -2,7 +2,6 @@ import {useState} from "react";
 import React from "react";
 import {
     Navbar,
-    MobileNav,
     Typography,
     Button,
     Menu,
@@ -10,22 +9,18 @@ import {
     MenuList,
     MenuItem,
     Avatar,
-    Card,
-    IconButton,
+    IconButton, Drawer,
 } from "@material-tailwind/react";
 import {
-    CubeTransparentIcon,
     UserCircleIcon,
-    CodeBracketSquareIcon,
-    Square3Stack3DIcon,
     ChevronDownIcon,
     Cog6ToothIcon,
     InboxArrowDownIcon,
     LifebuoyIcon,
     PowerIcon,
-    RocketLaunchIcon,
     Bars2Icon,
 } from "@heroicons/react/24/outline";
+
 
 // profile menu component
 const profileMenuItems = [
@@ -113,159 +108,68 @@ function ProfileMenu() {
     );
 }
 
-const navListMenuItems = [
-    {
-        title: "@material-tailwind/html",
-        description:
-            "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
-    },
-    {
-        title: "@material-tailwind/react",
-        description:
-            "Learn how to use @material-tailwind/react, packed with rich components for React.",
-    },
-    {
-        title: "Material Tailwind PRO",
-        description:
-            "A complete set of UI Elements for building faster websites in less time.",
-    },
-];
 
-function NavListMenu() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-    const renderItems = navListMenuItems.map(({title, description}) => (
-        <a href="#" key={title}>
-            <MenuItem>
-                <Typography variant="h6" color="blue-gray" className="mb-1">
-                    {title}
-                </Typography>
-                <Typography variant="small" color="gray" className="font-normal">
-                    {description}
-                </Typography>
-            </MenuItem>
-        </a>
-    ));
-
-    return (
-        <React.Fragment>
-            <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
-                <MenuHandler>
-                    <Typography as="a" href="#" variant="small" className="font-normal">
-                        <MenuItem className="hidden items-center gap-2 text-blue-gray-900 lg:flex lg:rounded-full">
-                            <Square3Stack3DIcon className="h-[18px] w-[18px] text-amber-300"/> Pages
-                            <ChevronDownIcon
-                                strokeWidth={2}
-                                className={`h-3 w-3 transition-transform ${
-                                    isMenuOpen ? "rotate-180" : ""
-                                }`}
-                            />
-                        </MenuItem>
-                    </Typography>
-                </MenuHandler>
-                <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid">
-                    <Card
-                        color="blue"
-                        shadow={false}
-                        variant="gradient"
-                        className="col-span-3 grid h-full w-full place-items-center rounded-md"
-                    >
-                        <RocketLaunchIcon strokeWidth={1} className="h-28 w-28"/>
-                    </Card>
-                    <ul className="col-span-4 flex w-full flex-col gap-1">
-                        {renderItems}
-                    </ul>
-                </MenuList>
-            </Menu>
-            <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden text-amber-300">
-                <Square3Stack3DIcon className="h-[18px] w-[18px] text-amber-300"/> Pages
-            </MenuItem>
-            <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
-                {renderItems}
-            </ul>
-        </React.Fragment>
-    );
-}
-
-// nav list component
-const navListItems = [
-    {
-        label: "Account",
-        icon: UserCircleIcon,
-    },
-    {
-        label: "Blocks",
-        icon: CubeTransparentIcon,
-    },
-    {
-        label: "Docs",
-        icon: CodeBracketSquareIcon,
-    },
-];
-
-function NavList() {
-    return (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
-            <NavListMenu/>
-            {navListItems.map(({label, icon}) => (
-                <Typography
-                    key={label}
-                    as="a"
-                    href="#"
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                >
-                    <MenuItem className="flex items-center gap-2 lg:rounded-full">
-                        {React.createElement(icon, {className: "h-[18px] w-[18px]"})}{" "}
-                        {label}
-                    </MenuItem>
-                </Typography>
-            ))}
-        </ul>
-    );
-}
 
 function Header() {
-    const [isNavOpen, setIsNavOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-    const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
-
-    React.useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setIsNavOpen(false),
-        );
-    }, []);
-
+    const openDrawer = () => setOpen(true);
+    const closeDrawer = () => setOpen(false);
     return (
-        <Navbar fullWidth={true} variant='gradient' className="mx-auto lg:pl-6 w-full" >
-            <div className="relative mx-auto flex items-center text-blue-gray-900">
-                <Typography
-                    as="a"
-                    href="#"
-                    className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
-                >
-                    Material Tailwind
-                </Typography>
-                <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
-                    <NavList/>
-                </div>
-                <IconButton
-                    size="sm"
-                    color="blue-gray"
-                    variant="text"
-                    onClick={toggleIsNavOpen}
-                    className="ml-auto mr-2 lg:hidden"
-                >
-                    <Bars2Icon className="h-6 w-6"/>
-                </IconButton>
-                <ProfileMenu/>
-            </div>
-            <MobileNav open={isNavOpen} className="overflow-scroll">
-                <NavList/>
-            </MobileNav>
-        </Navbar>
+      <nav className=''>
+          <Navbar fullWidth={true} variant='gradient' className="mx-auto lg:pl-6 w-full">
+              <div className="relative mx-auto flex items-center text-blue-gray-900">
+                  <img
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXGky50hqnXw4OspoYR8ENebsJzE6Cd1lYeBOBrIelVE4BfEQTUqNzz_7T1EB1TSgUag&usqp=CAU"
+                      alt="Your Logo"
+                      className="mr-4 h-12 w-auto"/>
+
+                  <IconButton
+                      size="sm"
+                      color="blue-gray"
+                      variant="text"
+                      onClick={openDrawer}
+                      className="ml-auto mr-2 lg:hidden"
+                  >
+                      <Bars2Icon className="h-6 w-6"/>
+                  </IconButton>
+                  <ProfileMenu/>
+              </div>
+          </Navbar>
+          <Drawer open={open} onClose={closeDrawer} className="p-4">
+              <div className="mb-6 flex items-center justify-between">
+                  <Typography variant="h5" color="blue-gray">
+                      Material Tailwind
+                  </Typography>
+                  <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2}
+                          stroke="currentColor"
+                          className="h-5 w-5"
+                      >
+                          <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6 18L18 6M6 6l12 12"
+                          />
+                      </svg>
+                  </IconButton>
+              </div>
+              <Typography color="gray" className="mb-8 pr-4 font-normal">
+                  Material Tailwind features multiple React and HTML components, all
+                  written with Tailwind CSS classes and Material Design guidelines.
+              </Typography>
+              <div className="flex gap-2">
+                  <Button size="sm">Get Started</Button>
+                  <Button size="sm" variant="outlined">
+                      Documentation
+                  </Button>
+              </div>
+          </Drawer>
+      </nav>
     );
 }
 
